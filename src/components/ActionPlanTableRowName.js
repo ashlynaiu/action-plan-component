843e0eb839bd886a73a3e662dee8cb614f58f07e
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2017-01-19 13:42:35 -0800

    Fix chevron and separate cell into new component

---
+++ src/components/ActionPlanTableRowName.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import chevronDown from '../images/icons/chevrondown.svg';
+
+class ActionPlanTableRowName extends React.Component {
+	constructor() {
+		super();
+		this.clickHandler = this.clickHandler.bind(this);
+		this.transformChevron = this.transformChevron.bind(this);
+		this.state = {
+			chevronCollapsed: false
+		}
+	}
+
+	clickHandler(index) {
+		this.props.collapseChildren(index);
+		this.transformChevron();
+	}
+
+	transformChevron() {
+		if(this.props.data.dependentChildren === true) {
+			let chevron = this.state.chevronCollapsed;
+			chevron = !chevron;
+			this.setState({ chevronCollapsed: chevron})
+		}
+	}
+
+	render() {
+		const { data, index } = this.props;
+
+		//Calculate the indentation of a row by adding the appropriate class
+		let indentCal = ()=> {
+			if(data.dependentParent > 0) {
+				return `dataTable-dependent--${data.nested}`;
+			}
+			return 'dataTable-dependent';
+		};
+
+		//Calculate if the row should have a Chevron
+		let showChevron = ()=> {
+			if(data.dependentChildren === true) {
+				return (
+					<use xlinkHref={chevronDown + '#chevrondown'}></use>
+				)
+			}
+		};
+
+		//Calculate transform of chevron based on state
+		let chevronCSS = ()=> {
+			if(data.dependentChildren === true) {
+				if(this.state.chevronCollapsed) {
+						return 'chevron-closed';
+					}
+					else {
+						return 'chevron-open';
+					}
+				}
+		}
+
+		return (
+			<td scope="row" className={indentCal()} data-label={data.name}>
+				<div className="slds-th__action slds-th__action--form">
+					<div className={`dataTable-chevronDown ${chevronCSS()}`} onClick={() => this.clickHandler(index)}>
+						<svg className="slds-button__icon--small" viewBox="0 0 52 52" aria-hidden="true">
+							{showChevron()}
+						</svg>
+					</div>
+					<span className="slds-checkbox">
+						<input type="checkbox" name="options" id={`checkbox-${index}`} />
+						<label className="slds-checkbox__label" htmlFor={`checkbox-${index}`}>
+							<span className="slds-checkbox--faux"></span>
+							<span className="slds-form-element__label slds-assistive-text">Select All</span>
+						</label>
+					</span>
+				</div>
+				<div className="slds-truncate" title={data.name}>{data.name}</div>
+			</td>
+		)
+	}
+}
+
+export default ActionPlanTableRowName;
