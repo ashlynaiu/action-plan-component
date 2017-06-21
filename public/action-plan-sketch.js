6af38bc9648ea98d325dbf08276a9d5a5f75473c
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2017-06-21 11:31:26 -0700

    json test files

---
+++ public/action-plan-sketch.js
@@ -0,0 +1,75 @@
+[
+	{
+		"name": "Call Prospect to setup a meeting",
+		"priority": "High",
+		"days": 1,
+		"assigned": "Associate",
+		"category": "Phone call",
+		"compliance": true
+	},
+	{
+		"name": "Prospect meeting",
+		"priority": "High",
+		"days": 5,
+		"assigned": "Advisor",
+		"category": "Meeting",
+		"compliance": true
+	},
+	{
+		"name": "Log meeting notes into Salesforce",
+		"priority": "High",
+		"days": 2,
+		"assigned": "Advisor",
+		"category": "Misc",
+		"compliance": true
+	},
+	{
+		"name": "Update client info in CRM",
+		"priority": "Medium",
+		"days": 2,
+		"assigned": "Associate",
+		"category": "Misc",
+		"compliance": false
+	},
+	{
+		"name": "Follow up email",
+		"priority": "High",
+		"days": 1,
+		"assigned": "Associate",
+		"category": "Email",
+		"compliance": true
+	},
+	{
+		"name": "Prepare paperwork to send to client.",
+		"priority": "High",
+		"days": 5,
+		"assigned": "Associate",
+		"category": "Email",
+		"compliance": true
+	},
+	{
+		"name": "Submit paperwork",
+		"priority": "High",
+		"edit": "Everyone",
+		"days": 7,
+		"assigned": "Associate",
+		"category": "Misc",
+		"compliance": true
+	},
+	{
+		"name": "Apply follow up in a quarter task template",
+		"priority": "High",
+		"days": 2,
+		"assigned": "Advisor",
+		"category": "Misc",
+		"compliance": false
+	},
+	{
+		"name": "Attach Setting Client Expectation survey and template",
+		"priority": "Medium",
+		"days": 2,
+		"assigned": "Associate",
+		"category": "Misc",
+		"compliance": true
+	}
+]
